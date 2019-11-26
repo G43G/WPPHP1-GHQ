7063e9208ee018df06fdd5400d70be31549d1f2f
Bogdan Matorkić <bogdan_992@outlook.com>
2019-11-26 19:55:47 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,20 +1,26 @@
 var slideIndex = 0;
-showSlides();
+
 function showSlides()
 {
     var i;
     var slides = document.getElementsByClassName("slide");
-    for (i = 0; i < slides.length; i++)
-	{
-        slides[i].style.display = "none";
+
+    if(slides.length) {
+         for (i = 0; i < slides.length; i++)
+	 {
+             slides[i].style.display = "none";
+         }
+	    
+         slideIndex++;
+	    
+         if(slideIndex > slides.length)
+	 {
+	     slideIndex = 1
+	 }
+	    
+         slides[slideIndex-1].style.display = "block";
+         setTimeout(showSlides, 3000);
     }
-    slideIndex++;
-    if (slideIndex> slides.length)
-	{
-		slideIndex = 1
-	}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000);
 }
 
 function reveal()
